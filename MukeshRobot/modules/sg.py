c7483a5ab69a06f73feadf230f45f9fb07d1fcdd
ROBOBOTOP <ga69582765@gmail.com>
2024-06-08 22:02:36 +0530

    Update sg.py

--- MukeshRobot/modules/sg.py
+++ MukeshRobot/modules/sg.py
@@ -1,3 +1,9 @@
+from pyrogram import filters
+
+from MukeshRobot import pbot
+from MukeshRobot.utils.errors import capture_err
+from MukeshRobot.modules.helper_funcs.misc import is_module_loaded
+
 help = """
 
 ๏ /sg ᴜsᴇʀ ᴄʜᴀᴋ ᴀʟʟ ɴᴀᴍᴇs
