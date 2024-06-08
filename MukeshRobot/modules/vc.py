76ca98dcfa4454b998b1f327b4a684d1c595585e
ROBOBOTOP <ga69582765@gmail.com>
2024-06-08 22:54:08 +0530

    Update vc.py

--- MukeshRobot/modules/vc.py
+++ MukeshRobot/modules/vc.py
@@ -4,12 +4,13 @@ from MukeshRobot import pbot
 from MukeshRobot.utils.errors import capture_err
 from MukeshRobot.modules.helper_funcs.misc import is_module_loaded
 
-help = """
+__mod_name__ = "ᴠᴄ ᴛᴏᴏʟs"
 
+__help__ = """
+**ᴠᴄ ᴛᴏᴏʟs**
 ๏ /startvc - sᴛᴀʀᴛ ᴛʜᴇ ᴠᴄ [ᴍᴀᴋᴇ sᴜʀᴇ ᴍᴜsɪᴄ ᴀssɪsɪᴛᴀɴᴛ ɪs ᴀɴ ᴀᴅᴍɪɴ ᴡɪᴛʜ ᴍᴀɴᴀɢᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴘᴇʀᴍɪssɪᴏɴ]
 ๏ /vcend - Eɴᴅ ᴛʜᴇ ᴠᴄ [ᴍᴀᴋᴇ sᴜʀᴇ ᴍᴜsɪᴄ ᴀssɪsɪᴛᴀɴᴛ ɪs ᴀɴ ᴀᴅᴍɪɴ ᴡɪᴛʜ ᴍᴀɴᴀɢᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴘᴇʀᴍɪssɪᴏɴ]
 ๏ /vclink - ɢᴇᴛ ᴠᴏɪᴄᴇᴄʜᴀᴛ ʟɪɴᴋ
 ๏ /vcmembers - Gᴇᴛ ᴍᴇᴍᴇʙᴇʀ ʟɪsᴛ ᴛʜᴀᴛ ɪs ɪɴ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ .
-"""
 
-mod_name = "ᴠᴄ ᴛᴏᴏʟs"
+"""
