fd99d6e4918c71bd3d323282e67a5cdafdede513
ROBOBOTOP <ga69582765@gmail.com>
2024-06-08 23:10:43 +0530

    Update sg.py

--- MukeshRobot/modules/sg.py
+++ MukeshRobot/modules/sg.py
@@ -4,11 +4,12 @@ from MukeshRobot import pbot
 from MukeshRobot.utils.errors import capture_err
 from MukeshRobot.modules.helper_funcs.misc import is_module_loaded
 
-help = """
+__mod_name__ = "sᴀɴɢᴍᴀᴛᴀ"
 
-๏ /sg ᴜsᴇʀ ᴄʜᴀᴋ ᴀʟʟ ɴᴀᴍᴇs
+__help__ = """
+**sᴀɴɢᴍᴀᴛᴀ**
+❍ /sg ᴜsᴇʀ ᴄʜᴀᴋ ᴀʟʟ ɴᴀᴍᴇs
 
-๏ /ssg ᴏɴ (  ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴜsᴇ ɪɴ ɢʀᴏᴜᴘ ᴀɴʏ ᴜsᴇʀ ɴᴀᴍᴇ ᴄʜᴀɴɢᴇ ᴘʀᴇᴛᴇɴᴅᴇʀ ᴅᴇᴛᴇᴄᴛᴇᴅ .
+❍ /ssg ᴏɴ (  ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴜsᴇ ɪɴ ɢʀᴏᴜᴘ ᴀɴʏ ᴜsᴇʀ ɴᴀᴍᴇ ᴄʜᴀɴɢᴇ ᴘʀᴇᴛᴇɴᴅᴇʀ ᴅᴇᴛᴇᴄᴛᴇᴅ .
+
 """
-
-mod_name = "sᴀɴɢᴍᴀᴛᴀ"
